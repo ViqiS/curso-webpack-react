b7091e5fc3f51b0c1eac15e45287d460230be9c9
Piki <viqiescalada@gmail.com>
2023-03-24 14:14:07 -0300

    corregi typo

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
     ],
-    optimizacion: {
+    optimization: {
         minimize: true,
         minimizer: [
             new CssMinimizerPlugin(),
